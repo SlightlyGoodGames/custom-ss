722c35a501d6014fe059e101d30b0ba8623e6eff
George Skudder <george.skudder@hotmail.com>
2026-04-18 09:11:41 +0100

    Added beta screensaver cycling (v0.2.21)

--- generalscript.py
+++ generalscript.py
@@ -1,7 +1,13 @@
 import os,yaml
 
 with open("C:/Screensavers/globalconfig.yaml","r") as f:
-    filename = yaml.safe_load(f)["screensaver"]
+    loadyaml = yaml.safe_load(f)
+filename = loadyaml["cycle"][loadyaml["screensaver"]]
+loadyaml["screensaver"] += 1
+if loadyaml["screensaver"] == len(loadyaml["cycle"]):
+    loadyaml["screensaver"] = 0
+with open("C:/Screensavers/globalconfig.yaml","w") as f:
+    yaml.dump(loadyaml,f)
 try:
     os.startfile("C:/Screensavers/" + filename + "/scrnsave.exe")
 except:
